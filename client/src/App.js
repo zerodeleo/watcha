8b501ac2f6a77c15699c1b62365b0de843b405c3
jetris zerodeleo <julia.stjerna@hotmail.se>
2022-07-04 19:39:46 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import './css/index.css';
+import axios from 'axios';
+
+function App() {
+  const uid = 1;
+
+  const onClick = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+
+    switch (name) {
+      case 'GET':
+        axios.get(`api/${uid}`)
+          .then(res => console.log('response: ', res))
+          .catch(err => console.log(err));
+        break;
+      case 'POST':
+        axios.post(`api/post/${uid}`, { document: 'hello database' })
+          .then(res => console.log('response: ', res))
+          .catch(err => console.log(err));
+        break;  
+      default:
+        console.log('Nothing happened');
+        break;
+    }
+
+
+  }
+
+  return (
+    <div className="App">
+      <button onClick={ onClick } name='GET'>GET</button>
+      <button onClick={ onClick } name='POST'>POST</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+require("dotenv").config();
+
+//config body-parser to post data
+const cors = require('cors');
+const bodyParser = require('body-parser')
+app.use(cors())
+app.use(bodyParser.urlencoded({ limit: '50mb', extended:true }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(cors())
+
+// Connect to MongoDB
+const mongoose = require('mongoose');
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { useNewUrlParser: true })
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully")
+})
+
+const dbRouter = require('./src/routes/db');
+app.use('/api', dbRouter);
+
+// Create build folder during deployment, two
+const path = require('path')
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+}
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Charlie is running on port ${PORT}`);
+})
